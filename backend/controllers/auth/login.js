7dcf492cb0fad3bc6500c03eedffb8e229227641
ali <ali.ergul@pusula.net.tr>
2025-01-20 16:53:16 +0300

    changed login response

--- backend/controllers/auth/login.js
+++ backend/controllers/auth/login.js
@@ -38,7 +38,7 @@ exports.login = async (req, res) => {
         username: userInfo.username,
         email: userInfo.email,
       },
-      accessToken,
+      access_token: accessToken,
     });
   } catch (error) {
     return sendErrorResponse(
